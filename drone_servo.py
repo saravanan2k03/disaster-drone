43b84a34797dd4bc406c33ca0e1132345494c6be
sens-sens <90544639+sens-sens@users.noreply.github.com>
2024-09-21 12:57:26 +0530

    Update drone_servo.py

--- drone_servo.py
+++ drone_servo.py
@@ -1,22 +1,22 @@
-from gpiozero import Servo
+from gpiozero import AngularServo
 from time import sleep
 
 # Use GPIO pin 22
-servo = Servo(22)
+servo = AngularServo(22, min_angle=0, max_angle=180)
 
 def dropPackage():
     try:
         print('Servo started...')
         sleep(1)
-        setAngle(1)  # set to 180 degrees
+        setAngle(180)  # set to 180 degrees
         sleep(5)
-        setAngle(-1)  # set to 0 degrees
+        setAngle(0)    # set to 0 degrees
     except KeyboardInterrupt:
         print('Servo interrupted..')
 
-def setAngle(position):
-    servo.value = position
+def setAngle(angle):
+    servo.angle = angle
     sleep(1)
 
-# Start at 0 degrees (equivalent to position -1)
-setAngle(-1)
+# Start at 0 degrees
+setAngle(0)
